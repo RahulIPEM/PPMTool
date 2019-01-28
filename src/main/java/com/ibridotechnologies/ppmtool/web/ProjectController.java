aca16a8d012d6804dd3c8ab053f4f439f1e0faa9
Rahul Sharma <rs5802@gmail.com>
2019-01-28 23:05:26 +0530

    service and web api endpoint/controller added

---
+++ src/main/java/com/ibridotechnologies/ppmtool/web/ProjectController.java
@@ -0,0 +1,26 @@
+package com.ibridotechnologies.ppmtool.web;
+
+import com.ibridotechnologies.ppmtool.domain.Project;
+import com.ibridotechnologies.ppmtool.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/project")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("")
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+
+}
