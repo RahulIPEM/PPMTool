e43649392d4670e921345901f3c117719cf97f64
Rahul Sharma <rs5802@gmail.com>
2019-02-20 01:05:10 +0530

    WIP: ppmtool react client setup

--- PPMToolFullStack/src/main/java/com/rahul/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/rahul/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.rahul.ppmtool.services;
 
 import com.rahul.ppmtool.domain.Project;
+import com.rahul.ppmtool.exceptions.ProjectIdException;
 import com.rahul.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,14 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     //Saving project object into DB
-    public Project saveOrUpdate(Project project){
-        //Logic
-        return projectRepository.save(project);
+    public Project saveOrUpdate(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception ex) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists.");
+        }
+
     }
 
 }
--- PPMToolFullStack/src/main/java/com/rahul/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/rahul/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.rahul.ppmtool.web;
 
 import com.rahul.ppmtool.domain.Project;
+import com.rahul.ppmtool.services.MapValidationErrorService;
 import com.rahul.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,15 +25,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
-        if(result.hasErrors()){
-            Map<String,String> errorMap = new HashMap<>();
-            for (FieldError error:result.getFieldErrors()){
-                errorMap.put(error.getField(),error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if(errorMap != null) return errorMap;
+
         projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
